76f4c64d0e3f328b8df9ff61a1c6ad26d5bcd31a
rabia <heerawarraich123@gmail.com>
2024-05-28 22:02:23 +0500

    final commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.webview;
+
+import static com.example.webview.R.id.webview;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+  private WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mWebView = findViewById(webview);
+        mWebView.setWebViewClient(new myWebViewclient()); // to handle URL redirects in the app
+        mWebView.getSettings().setJavaScriptEnabled(true); // to enable JavaScript on web pages
+        mWebView.getSettings().setGeolocationEnabled(true); // to enable GPS location on web pages
+        mWebView.loadUrl("https://www.amazon.com");
+
+    }
+    public class myWebViewclient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+
+            return true;
+        }
+
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+
+            final String urls = url;
+            if (urls.contains("mailto") || urls.contains("whatsapp") || urls.contains("tel") || urls.contains("sms") || urls.contains("facebook") || urls.contains("truecaller") || urls.contains("twiter")) {
+                mWebView.stopLoading();
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(urls));
+                startActivity(i);
+
+
+            }
+
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
